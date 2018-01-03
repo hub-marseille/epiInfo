c4c7cc3c2c6b894923d4d46e34514998d17a6097
belugu_m <belugu_m@epitech.eu>
2018-01-03 18:48:12 +0100

    Horaires de metro et tram

---
+++ epiInfoDisplay/rtminfo.h
@@ -0,0 +1,31 @@
+#ifndef RTMINFO_H
+#define RTMINFO_H
+
+#include <curl/curl.h>
+#include <iostream>
+#include <string>
+#include <QString>
+#include <QStringList>
+#include "json.hpp"
+
+using namespace nlohmann;
+using namespace std;
+
+#define NB_STOPS 2
+
+class RtmInfo
+{
+    QStringList         out_list;
+    string              *urls;
+
+public:
+    RtmInfo();
+    ~RtmInfo();
+    /*
+     * 0 = Euroméditérranée Gantès
+     * 1 = Désirée Clary
+     */
+    QStringList const   &getNextPassage(size_t);
+};
+
+#endif // RTMINFO_H
